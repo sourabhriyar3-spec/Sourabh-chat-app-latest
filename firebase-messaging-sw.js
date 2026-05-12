f648a2b2ae4a1f7745f58304d75450563f253678
Kuka <sourabhriyar3@gmail.com>
2026-05-12 14:26:26 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,6 @@
-// firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-// 🟢 Insert your specific Firebase keys here
 firebase.initializeApp({
   projectId: "kuku-171b0",
   databaseURL: "https://kuku-171b0-default-rtdb.asia-southeast1.firebasedatabase.app/",
@@ -12,14 +10,12 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-// This runs when the app is swiped closed in the background
 messaging.onBackgroundMessage(function(payload) {
+  console.log('Background message received ', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png', // Lock icon
-    badge: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png'
+    icon: payload.notification.icon
   };
-
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
